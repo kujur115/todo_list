942e20fcba5d78ae0f00aa172f94d6b45a23b1b8
Pratik Kujur <kujurpratik115@gmail.com>
2023-02-08 14:02:49 +0530

    setup controller for deletetask,deletedonetask,etc.. tried solving update

--- controllers/home.js
+++ controllers/home.js
@@ -1,21 +1,21 @@
-const task=require('../models/task');
+const Task=require('../models/task');
 
 
 module.exports.home=function(req,res){
     // return res.end('<h1>Express is up for Coderial</h1>');
     // console.log(task)
 
-    task.find({},(err, taskList)=>{
+    Task.find({},(err, taskList)=>{
         if(err){
             console.log('Error while fetching the data from db');
             return;
         }
 
         return res.render('home',{
-        title:"My TODO",
-        Task: taskList
+            title:"My TODO",
+            Task: taskList
+        });
     });
-    })
 
 
     // return res.render('home',{
@@ -23,3 +23,88 @@ module.exports.home=function(req,res){
     //     Task: task
     // });
 }
+
+module.exports.createTask=(req,res)=>{
+    Task.create({
+        task: req.body.task,
+        category: req.body.category,
+        date : req.body.date,
+        done : false
+
+    },(err)=>{
+        if(err){
+            console.log(`Error in creating a task: ${err}`);
+            return;
+        }
+        // console.log('*******',newTask);
+        return res.redirect('back');
+    });
+    // console.log(req.body);
+
+};
+
+module.exports.deleteTask=function(req, res){
+    // console.log(req.query);
+    let id = req.query.id
+
+    Task.findOneAndDelete(id, function(err){
+        if(err){
+            console.log('error in deleting the object');
+            return;
+        }
+        return res.redirect('back');
+    });
+};
+
+module.exports.deleteDoneTask=(req,res)=>{
+
+    Task.deleteMany({done:true},(err)=>{
+        if(err){
+            console.log(`Error deleting done Task : ${err}`);
+            return;
+        }
+        console.log(`delete done task completed`);
+        return res.redirect('back');
+    })
+
+}
+
+module.exports.toggleTask= (req,res)=>{
+    let id = req.query.id;
+    // let t;
+    // console.log('fdjfdjfd');
+    // Task.findById(id,(err,task)=>{
+    //     if(err){
+    //         console.log(err);
+    //         return;
+    //     }
+    //     t=task.done;
+    //     // console.log(task);
+    //     t=(t)?false:true;
+    //     console.log(t,"8888888888888888");
+    // }).then(()=>{
+
+
+        Task.findOneAndUpdate(id,{$set:{done : {$eq:[false,"$present"]}}},(err)=>{
+            
+            if(err){
+                console.log('error in updating the object',err);
+                return;
+            }
+            console.log("update done");
+            // task.done=(task.done)?false:true;
+            return res.redirect('back');
+        });
+
+        // Task.findOneAndUpdate(id,{ $set:{done: ('$done')?false:true}},(err)=>{
+        //     if(err){
+        //         console.log('error in updating the object',err);
+        //         return;
+        //     }
+        //     console.log("update done");
+        //     // task.done=(task.done)?false:true;
+        //     return res.redirect('back');
+        // })
+
+
+    };
--- routes/index.js
+++ routes/index.js
@@ -8,78 +8,13 @@ const homeController=require('../controllers/home');
 
 router.get('/',homeController.home);
 
-router.post('/create-task',(req,res)=>{
-    Task.create({
-        task: req.body.task,
-        category: req.body.category,
-        date : req.body.date,
-        done : false
+router.post('/create-task',homeController.createTask);
 
-    },(err,newTask)=>{
-        if(err){
-            console.log(`Error in creating a task: ${err}`);
-            return;
-        }
-        console.log('*******',newTask);
-        return res.redirect('back');
-    });
-    // console.log(req.body);
+router.get('/toggle/',homeController.toggleTask);
 
-});
+router.get('/delete-task/', homeController.deleteTask);
 
-router.get('/toggle/',(req,res)=>{
-    let id = req.query.id;
-    // let t;
-    // console.log('fdjfdjfd');
-    // Task.findById(id,(err,task)=>{
-    //     if(err){
-    //         console.log(err);
-    //         return;
-    //     }
-    //     t=task.done;
-    //     // console.log(task);
-    //     t=(t)?false:true;
-    //     console.log(t,"8888888888888888");
-    // }).then(()=>{
-
-
-        Task.findOneAndUpdate(id,{$set:{done : {$not:"$done"}}},(err)=>{
-            
-            if(err){
-                console.log('error in updating the object',err);
-                return;
-            }
-            console.log("update done");
-            // task.done=(task.done)?false:true;
-            return res.redirect('back');
-        });
-
-
-    // })
-    
-    
-    
-    // Contact.findOneAn(id, function(err){
-    //     if(err){
-    //         console.log('error in deleting the object');
-    //         return;
-    //     }
-    //     return res.redirect('back');
-    // });
-});
-router.get('/delete-task/', function(req, res){
-    // console.log(req.query);
-    let id = req.query.id
-
-    Task.findOneAndDelete(id, function(err){
-        if(err){
-            console.log('error in deleting the object');
-            return;
-        }
-        return res.redirect('back');
-    });
-});
-
+router.get('/delete-done-task', homeController.deleteDoneTask);
 
 
 module.exports=router;
